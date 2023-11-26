507ae884878f95823596a4ba34b10f6f73121f03
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-11-26 15:18:15 +0530

    api called in frontend

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -4,5 +4,8 @@ const app = require("./APIs/app");
 
 const { PORT } = process.env || 8080;
 createServer(app).listen(PORT, () => {
-   console.log(`server has started on port ${PORT}: http://127.0.0.1:${PORT}/api/user`.yellow);
+   console.log(
+      `server has started on port ${PORT}: http://127.0.0.1:${PORT}/api/user`
+         .yellow
+   );
 });
