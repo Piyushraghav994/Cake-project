15b3168a592a4c0a671381540145c822153bf216
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-11-27 16:49:07 +0530

    worked on cart stuffs

---
+++ CAKE PROJECT/cartFunctionality.js
@@ -0,0 +1,5 @@
+console.log("testing..");
+const singleProductCart = document.getElementById("singleProductCart");
+singleProductCart.addEventListener("click", () => {
+   console.log("Clicking the cart");
+});
